16b8b4c1b6de52b9af40f8fb2aff52b25528274d
kalaivani163 <kalaigreen163@gmail.com>
2021-08-29 18:02:28 +0530

    My 1st push

---
+++ src/or/add/GreensTech.java
@@ -0,0 +1,30 @@
+package or.add;
+
+public class GreensTech {
+private void greensomr() {
+	System.out.println("greens omr in omr");
+
+}
+private void greensadayar() {
+	System.out.println("greens adayar in adayar");
+
+}
+private void greensthambaram() {
+	System.out.println("greens thambaram in thambaram");
+}
+private void greensvelachery() {
+	System.out.println("greens velachery in velacherry");
+}
+private void greensannagar() {
+	System.out.println("greens annanagar in annanagar");
+}
+public static void main(String[] args) {
+	GreensTech g = new GreensTech();
+	g.greensomr();
+	g.greensadayar();
+	g.greensthambaram();
+	g.greensvelachery();
+	g.greensannagar();
+	
+}
+}
